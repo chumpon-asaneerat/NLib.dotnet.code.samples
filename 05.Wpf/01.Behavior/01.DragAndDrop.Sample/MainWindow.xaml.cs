3cd0bb6cd49828ad300f29c6690faa0bd37e218b
chumpon-asaneerat <chumponsenate@gmail.com>
2022-08-04 00:56:15 +0700

    NLib.Net code samples  #7 - DragAndDrop.Sample project   - Test Drop #1.

--- 05.Wpf/01.Behavior/01.DragAndDrop.Sample/MainWindow.xaml.cs
+++ 05.Wpf/01.Behavior/01.DragAndDrop.Sample/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#region Using
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+#endregion
+
 namespace DragAndDropSample
 {
     /// <summary>
@@ -20,9 +24,73 @@ namespace DragAndDropSample
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Drag/Drop Handlers
+
+        private void imgSource_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+        {
+
+        }
+
+        private void imgSource_QueryContinueDrag(object sender, QueryContinueDragEventArgs e)
+        {
+
+        }
+
+        private void imgSource_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void imgSource_DragOver(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void imgSource_DragLeave(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void imgSource_Drop(object sender, DragEventArgs e)
+        {
+            /*
+            var droppedData = e.Data.GetData(typeof(Card)) as Card;
+            var target = (sender as ListBoxItem).DataContext as Card;
+
+            int targetIndex = CardListControl.Items.IndexOf(target);
+
+            droppedData.Effect = null;
+            droppedData.RenderTransform = null;
+
+            Items.Remove(droppedData);
+            Items.Insert(targetIndex, droppedData);
+
+            // remove the visual feedback drag and drop item
+            if (this._dragdropWindow != null)
+            {
+                this._dragdropWindow.Close();
+                this._dragdropWindow = null;
+            }
+            */
+        }
+
+        private void imgSource_PreviewDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        #endregion
     }
 }
