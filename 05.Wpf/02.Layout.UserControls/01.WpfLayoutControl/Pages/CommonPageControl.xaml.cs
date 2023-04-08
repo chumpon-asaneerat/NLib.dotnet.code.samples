cb6a7ca0ac146f76ac0b1cb415960635618b15ad
chumpon-asaneerat <chumponsenate@gmail.com>
2023-04-09 03:24:23 +0700

    NLib.Net code samples  #16 - Wpf Layout UserControl projects Update #3   - Implements Common Page Control #1.

---
+++ 05.Wpf/02.Layout.UserControls/01.WpfLayoutControl/Pages/CommonPageControl.xaml.cs
@@ -0,0 +1,67 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace WpfLayoutControl.Pages
+{
+    /// <summary>
+    /// Interaction logic for CommonPageControl.xaml
+    /// </summary>
+    [ContentProperty("WorkArea")]
+    public partial class CommonPageControl : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CommonPageControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region PageTitle
+
+        public static readonly DependencyProperty PageTitleProperty =
+            DependencyProperty.Register("PageTitle", typeof(string), typeof(CommonPageControl));
+
+        public string PageTitle
+        {
+            get { return (string)GetValue(PageTitleProperty); }
+            set { SetValue(PageTitleProperty, value); }
+        }
+
+        #endregion
+
+        #region WorkArea
+
+        public static readonly DependencyProperty WorkAreaProperty =
+            DependencyProperty.Register("WorkArea", typeof(object), typeof(CommonPageControl));
+
+        public object WorkArea
+        {
+            get { return (object)GetValue(WorkAreaProperty); }
+            set { SetValue(WorkAreaProperty, value); }
+        }
+
+        #endregion
+    }
+}
