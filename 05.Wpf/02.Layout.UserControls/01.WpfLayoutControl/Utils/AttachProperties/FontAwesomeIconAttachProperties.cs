38fbe792503b94716a2ef8c5cb60ef8e42ef6938
chumpon-asaneerat <chumponsenate@gmail.com>
2023-04-10 14:54:39 +0700

    NLib.Net code samples  #25 - Wpf Layout UserControl projects Update #11   - Implements Common Page Control #9.

---
+++ 05.Wpf/02.Layout.UserControls/01.WpfLayoutControl/Utils/AttachProperties/FontAwesomeIconAttachProperties.cs
@@ -0,0 +1,169 @@
+﻿#region Using
+
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+#endregion
+
+namespace WpfLayoutControl.Utils
+{
+    #region FontAwesome Enum
+
+    /// <summary>
+    /// The FontAwesomeIcon Enum.
+    /// </summary>
+    public enum FontAwesomeIcon
+    {
+        None,
+        Home,
+        Save,
+        Delete,
+        DeleteAll,
+        Import,
+        Export,
+        Print,
+        Preview,
+        Ok,
+        Cancel
+    }
+
+    #endregion
+
+    #region FontAwesomeOptions
+
+    /// <summary>
+    /// The FontAwesomeOptions class.
+    /// </summary>
+    public class FontAwesomeOptions
+    {
+        #region IconType
+
+        /// <summary>The IconType variable</summary>
+        public static readonly DependencyProperty IconTypeProperty = DependencyProperty.RegisterAttached(
+            "IconType",
+            typeof(FontAwesomeIcon),
+            typeof(FontAwesomeOptions),
+            new PropertyMetadata(FontAwesomeIcon.None, IconTypePropertyChanged));
+
+        /// <summary>
+        /// Gets IconType Value.
+        /// </summary>
+        /// <param name="obj">The target object.</param>
+        /// <returns>Returns current proeprty value.</returns>
+        [AttachedPropertyBrowsableForChildren(IncludeDescendants = false)]
+        [AttachedPropertyBrowsableForType(typeof(TextBlock))]
+        public static FontAwesomeIcon GetIconType(DependencyObject obj)
+        {
+            return (FontAwesomeIcon)obj.GetValue(IconTypeProperty);
+        }
+        /// <summary>
+        /// Sets IconType Value.
+        /// </summary>
+        /// <param name="obj">The target object.</param>
+        /// <param name="value">The new value.</param>
+        public static void SetIconType(DependencyObject obj, FontAwesomeIcon value)
+        {
+            obj.SetValue(IconTypeProperty, value);
+        }
+
+        #endregion
+
+        #region IconType Changed Handler
+
+        private static void IconTypePropertyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if (null != obj && obj is TextBlock)
+            {
+                TextBlock ctrl = obj as TextBlock;
+                string sVal = (null != e.NewValue) ? e.NewValue.ToString() : null;
+                FontAwesomeIcon val;
+                try
+                {
+                    val = (string.IsNullOrEmpty(sVal)) ? FontAwesomeIcon.None :
+                        (FontAwesomeIcon)Enum.Parse(typeof(FontAwesomeIcon), sVal);
+                }
+                catch (Exception) 
+                { 
+                    val = FontAwesomeIcon.None; 
+                }
+
+                switch (val)
+                {
+                    case FontAwesomeIcon.Home:
+                        {
+                            var style = (Style)Application.Current.Resources["fa-home"];
+                            ctrl.Style = style;
+                        }
+                        break;
+                    case FontAwesomeIcon.Save:
+                        {
+
+                        }
+                        break;
+                    case FontAwesomeIcon.Delete:
+                        {
+
+                        }
+                        break;
+                    case FontAwesomeIcon.DeleteAll:
+                        {
+
+                        }
+                        break;
+                    case FontAwesomeIcon.Import:
+                        {
+
+                        }
+                        break;
+                    case FontAwesomeIcon.Export:
+                        {
+
+                        }
+                        break;
+                    case FontAwesomeIcon.Print:
+                        {
+
+                        }
+                        break;
+                    case FontAwesomeIcon.Preview:
+                        {
+
+                        }
+                        break;
+                    case FontAwesomeIcon.Ok:
+                        {
+
+                        }
+                        break;
+                    case FontAwesomeIcon.Cancel:
+                        {
+
+                        }
+                        break;
+                    default:
+                        {
+                            // FontAwesomeIcon.None
+                        }
+                        break;
+                }
+
+                if (val == FontAwesomeIcon.None)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
