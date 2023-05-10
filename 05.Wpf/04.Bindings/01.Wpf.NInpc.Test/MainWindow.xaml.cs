5ef5b80af0a4c517dfa8bc68d688e8c0d2ca5a94
chumpon-asaneerat <chumponsenate@gmail.com>
2023-05-10 19:05:25 +0700

    NLib.Net code samples  #55 - Update Wpf.NInpc project #2.

--- 05.Wpf/04.Bindings/01.Wpf.NInpc.Test/MainWindow.xaml.cs
+++ 05.Wpf/04.Bindings/01.Wpf.NInpc.Test/MainWindow.xaml.cs
@@ -36,5 +36,45 @@ namespace Wpf.NInpc.Test
         }
 
         #endregion
+
+        #region Loaded/Closing
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitItems();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void InitItems()
+        {
+            lv.ItemsSource = Demo.GetItems();
+        }
+
+        #endregion
+    }
+
+    public class Demo : NLib.NInpc
+    {
+        public int Id { get; set; }
+
+        public static List<Demo> GetItems(int iMax = 10)
+        {
+            var rets = new List<Demo>();
+
+            for (int i = 0; i < iMax; ++i)
+            {
+                rets.Add(new Demo() { Id = i });
+            }
+
+            return rets;
+        }
     }
 }
