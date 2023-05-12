71dd58c387451c48b883ac4a0b19ae9c9b862361
chumpon-asaneerat <chumponsenate@gmail.com>
2023-05-12 22:21:26 +0700

    NLib.Net code samples  #71 - Update NInpc class (NLib.Utils) #1.

--- 00.NLib/NLib.Utils/Common/NInpc.cs
+++ 00.NLib/NLib.Utils/Common/NInpc.cs
@@ -1,8 +1,11 @@
 ﻿#region Using
 
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
 //using System.Collections.Generic;
 //using System.Runtime.CompilerServices; // .NET 4.5 required.
 //using NLib;
@@ -19,9 +22,102 @@ namespace NLib
     /// </summary>
     public abstract class NInpc : INotifyPropertyChanged
     {
+        #region Internal classes
+
+        abstract class NProperty { }
+
+        class NProperty<T> : NProperty
+        {
+            #region Public Properties
+
+            /// <summary>Gets or sets Value.</summary>
+            public T Value { get; set; }
+
+            #endregion
+        }
+
+        class NProperties
+        {
+            #region Internal Variables
+
+            private object olock = new object();
+            private Dictionary<string, NProperty> _properties = new Dictionary<string, NProperty>();
+
+            #endregion
+
+            #region Public Methods
+
+            /// <summary>
+            /// Gets Value.
+            /// </summary>
+            /// <typeparam name="T">The target proeprty type.</typeparam>
+            /// <param name="proopertyName">The Property Name.</param>
+            /// <returns>Returns Property value.</returns>
+            /// <exception cref="ArgumentNullException"></exception>
+            public T Get<T>(string proopertyName)
+            {
+                if (string.IsNullOrWhiteSpace(proopertyName))
+                    throw new ArgumentNullException(nameof(proopertyName));
+
+                lock (olock)
+                {
+                    if (!_properties.TryGetValue(proopertyName, out var _))
+                    {
+                        var p = new NProperty<T>() { Value = default };
+                        _properties.Add(proopertyName, p);
+                    }
+
+                    var inst = _properties[proopertyName] as NProperty<T>;
+                    return (null != inst) ? inst.Value : default;
+                }
+            }
+            /// <summary>
+            /// Set Value.
+            /// </summary>
+            /// <typeparam name="T">The target proeprty type.</typeparam>
+            /// <param name="proopertyName">The Property Name.</param>
+            /// <param name="value"></param>
+            /// <returns>Returns True if assigned value is not equal to original value.</returns>
+            /// <exception cref="ArgumentNullException"></exception>
+            public bool Set<T>(string proopertyName, T value)
+            {
+                if (string.IsNullOrWhiteSpace(proopertyName))
+                    throw new ArgumentNullException(nameof(proopertyName));
+
+                lock (olock)
+                {
+                    bool bChanged = false;
+                    if (!_properties.TryGetValue(proopertyName, out _))
+                    {
+                        var p = new NProperty<T>() { Value = value };
+                        _properties.Add(proopertyName, p);
+                        bChanged = true;
+                    }
+                    else
+                    {
+                        var inst = _properties[proopertyName] as NProperty<T>;
+                        if (null != inst)
+                        {
+                            if (!inst.Value.Equals(value))
+                            {
+                                inst.Value = value;
+                            }
+                            bChanged = true;
+                        }
+                    }
+                    return bChanged;
+                }
+            }
+
+            #endregion
+        }
+
+        #endregion
+
         #region Internal Variables
 
         private bool _lock = false;
+        private NProperties _properties = new NProperties();
 
         #endregion
 
@@ -62,6 +158,49 @@ namespace NLib
 
         #endregion
 
+        #region Protected Methods
+
+        /// <summary>
+        /// Get Value.
+        /// </summary>
+        /// <typeparam name="T">The Target Type.</typeparam>
+        /// <param name="propertyName">The Property Name.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        protected T Get<T>([CallerMemberName] string propertyName = null)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+                throw new ArgumentNullException(nameof(propertyName));
+
+            return _properties.Get<T>(propertyName);
+        }
+        /// <summary>
+        /// Set Value.
+        /// </summary>
+        /// <typeparam name="T">The Target Type.</typeparam>
+        /// <param name="Value">The Value to set.</param>
+        /// <param name="action">The action to execute when Value is change.</param>
+        /// <param name="propertyName">The Property Name.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        protected void Set<T>(T Value, Action action = null,
+            [CallerMemberName] string propertyName = null)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+                throw new ArgumentNullException(nameof(propertyName));
+
+            if (_properties.Set<T>(propertyName, Value))
+            {
+                Raise(propertyName); // Raise PropertyChanged
+                if (null != action)
+                {
+                    // do some extra action.
+                    action.Call();
+                }
+            }
+        }
+
+        #endregion
+
         #region Public Methods
 
         /// <summary>
@@ -82,8 +221,9 @@ namespace NLib
         /// Raise Property Changed event.
         /// </summary>
         /// <param name="propertyName">The property name.</param>
-        public void Raise(string propertyName)
+        public void Raise([CallerMemberName] string propertyName = null)
         {
+            if (string.IsNullOrEmpty(propertyName)) return;
             if (_lock) return; // if lock do nothing
             // raise event.
             PropertyChanged.Call(this, new PropertyChangedEventArgs(propertyName));
