e3e29b9064b3882c2a971abe337d2902acdfa443
chumpon-asaneerat <chumponsenate@gmail.com>
2023-05-22 18:50:10 +0700

    NLib.Net code samples  #87 - Update Wpf Custom Controls prject #5.

--- 05.Wpf/03.Controls/02.Wpf.CustomControls/02.Wpf.CustomControls/NUserPage.xaml.cs
+++ 05.Wpf/03.Controls/02.Wpf.CustomControls/02.Wpf.CustomControls/NUserPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿#region Using
+
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +11,14 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+#endregion
 
 namespace Wpf.Controls
 {
@@ -20,9 +27,56 @@ namespace Wpf.Controls
     /// </summary>
     public partial class NUserPage : UserControl
     {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public NUserPage()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Public Properties
+
+        #region PageTitle
+
+        /// <summary>
+        /// The PageTitleProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty PageTitleProperty =
+            DependencyProperty.Register("PageTitle", typeof(string), typeof(NUserPage));
+        /// <summary>
+        /// Gets or sets Page Title.
+        /// </summary>
+        public string PageTitle
+        {
+            get { return (string)GetValue(PageTitleProperty); }
+            set { SetValue(PageTitleProperty, value); }
+        }
+
+        #endregion
+
+        #region WorkArea
+
+        /// <summary>
+        /// The WorkAreaProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty WorkAreaProperty =
+            DependencyProperty.Register("WorkArea", typeof(object), typeof(NUserPage));
+        /// <summary>
+        /// Gets or sets WorkArea Content.
+        /// </summary>
+        public object WorkArea
+        {
+            get { return (object)GetValue(WorkAreaProperty); }
+            set { SetValue(WorkAreaProperty, value); }
+        }
+
+        #endregion
+
+        #endregion
     }
 }
